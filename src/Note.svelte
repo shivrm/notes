90ae9836fdc0d550b0c76be0e8feb0386dd2cfd3
ShivramS2006 <shivrams.2006@gmail.com>
2021-11-15 18:35:56 +0530

    Implement Note Component

---
+++ src/Note.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let content = "Hi";
+</script>
+
+<article class="note">
+    <ul class="toolbar">
+    </ul>
+    <div class="content">
+        {content}
+    </div>
+</article>
