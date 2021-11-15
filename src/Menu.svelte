08e3bdee889122a2f357f12e0534c3b7fe51bf67
ShivramS2006 <shivrams.2006@gmail.com>
2021-11-15 19:14:27 +0530

    Implement Menu Component

---
+++ src/Menu.svelte
@@ -0,0 +1,14 @@
+<nav>
+    <ul>
+        <li class="nav_element">
+            <button>
+                Add Note
+            </button>
+        </li>
+        <li class="nav_element">
+            <button>
+                Options
+            </button>
+        </li>
+    </ul>
+</nav>
