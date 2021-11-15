fa6eae793fdf8170419b964d5211ce81f2bccf55
ShivramS2006 <shivrams.2006@gmail.com>
2021-11-15 17:54:37 +0530

    Add header to App

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap');
+    header h1 {
+        font-family: 'Poppins', sans-serif;
+        font-size: 4em;
+        text-align: center;
+    }
+</style>
+
+<header>
+    <h1>
+        Notes
+    </h1>
+</header>
