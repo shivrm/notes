aa17eca9e0b4778c403dcc25ea476e86f981db9d
ShivramS2006 <shivrams.2006@gmail.com>
2021-11-16 16:11:58 +0530

    Add test notes

--- src/stores.js
+++ src/stores.js
@@ -1,3 +1,10 @@
 import { writable } from "svelte/store";
 
-export const notes = writable([])
+export const notes = writable([
+    {'content': "Note 1"},
+    {'content': "Note 2"},
+    {'content': "Note 3"},
+    {'content': "Note 4"},
+    {'content': "Note 5"},
+    {'content': "Note 6"}
+])
