14950f6ffb40467922bb99e45a2f862adae55bd4
ShivramS2006 <shivrams.2006@gmail.com>
2021-11-16 18:30:35 +0530

    Add delete button to Note toolbar element

--- src/Note.svelte
+++ src/Note.svelte
@@ -18,10 +18,28 @@
     }
 
     .toolbar {
-        border-radius: 1.5em 1.5em 0 0;
-        height: 20%;
         background-color: #f8fa6f;
+        border-radius: 1.5em 1.5em 0 0;
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        height: 20%;
         margin: 0;
+        padding: 0 1em;
+    }
+    .toolbar-item {
+        list-style-type: none;
+        height: 50%;
+        
+    }    
+
+    .toolbar-item svg {
+        min-height: 100%;
+        transition: 0.1s;
+    }
+
+    .toolbar-item:hover svg {
+        transform: scale(1.1);
     }
 
     .body {
@@ -41,6 +59,9 @@
 
 <article class="note">
     <ul class="toolbar">
+        <li class="toolbar-item" title="Delete this Note" on:click={deleteSelf}>
+            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash fa-w-14" role="img" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"/></svg>
+        </li>
     </ul>
     <div class="body">
         <div class="content">
