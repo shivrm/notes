2ee99a5a3e06509925522f750ddce53ea765ab62
ShivramS2006 <shivrams.2006@gmail.com>
2021-11-18 17:05:23 +0530

    Add local storage of notes to stores.js

--- src/stores.js
+++ src/stores.js
@@ -1,16 +1,18 @@
 import { writable } from "svelte/store";
 
-export const notes = writable([
-    {'content': "Note 1"},
-    {'content': "Note 2"},
-    {'content': "Note 3"},
-    {'content': "Note 4"},
-    {'content': "Note 5"},
-    {'content': "Note 6"}
-])
+var storedNotes = JSON.parse(
+    localStorage.getItem('notes')
+) || []
+
+export const notes = writable(storedNotes)
 
 var notesArr;
 notes.subscribe((value) => {
+    localStorage.setItem(
+        'notes',
+        JSON.stringify(value)
+    )
+
     notesArr = value;
 });
 
