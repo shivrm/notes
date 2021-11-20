3f20970b75cf651ba16ae69c83b14a2ec4558262
ShivramS2006 <shivrams.2006@gmail.com>
2021-11-20 14:16:30 +0530

    Add markdown function to markdown.js

--- src/markdown.js
+++ src/markdown.js
@@ -1,3 +1,6 @@
+import { micromark } from "micromark";
+import { gfm, gfmHtml } from "micromark-extension-gfm";
+
 function sanitizeHtml(text) {
     var sanitized = text.replace('&', '&amp;')
                     .replace('<', '&lt;')
@@ -6,4 +9,13 @@ function sanitizeHtml(text) {
                     .replace("'", '&#39;');
 
     return sanitized;
+}
+
+export function markdown(text) {
+    return  micromark(
+        sanitizeHtml(text), {
+            extensions: [gfm()],
+            htmlExtensions: [gfmHtml()]
+        }
+    )
 }
